fe022e29912e683132adf82275a36c31e9d46b37
Nejad Ihsan <nejadihsan@gmail.com>
2026-06-12 19:55:30 +0530

    Completed 1.Reset Button, 2.Invalid Inputs

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const volumeResultEl = document.getElementById("literEl")
 const massResultEl = document.getElementById("kilogramEl")
 const inputValueEl = document.getElementById("inputEl")
 const convertBtn = document.getElementById("convert-btn")
+const resetBtn = document.getElementById("reset-btn")
+const errorPar = document.getElementById("errorEl")
 // functions
 function resultInFeet(input){
   return (input * 3.281).toFixed(3)
@@ -28,8 +30,15 @@ function resultInPounds(input){
 function resultInKilograms(input){
   return (input / 2.204).toFixed(3)
 }
+errorPar.innerHTML = `<p>Enter a value between 0 and 1000</p>`
 convertBtn.addEventListener("click", function(){
     let inputValue = inputValueEl.value
+    if(isNaN(inputValue) || (inputValue <= 0)){
+      errorPar.style.color = "red";
+      errorPar.style.textShadow = "1px 1px 10px rgba(0,0,0,0.5)";
+      errorPar.innerHTML = `<p>Please input a valid number</p>`
+    }
+    else{
     lengthResultEl.innerHTML = `
     <p>
       ${inputValue} meters = ${resultInFeet(inputValue)} feet | ${inputValue} feet = ${resultInMeter(inputValue)} meters
@@ -42,4 +51,16 @@ convertBtn.addEventListener("click", function(){
     <p>
       ${inputValue} Kilograms = ${resultInPounds(inputValue)} Pounds | ${inputValue} Pounds = ${resultInKilograms(inputValue)} Kilograms
     </p>`
+    errorPar.innerHTML = `<p></p>`
+    }
+})
+resetBtn.addEventListener("click",function(){
+  lengthResultEl.innerHTML = `<p>Please Enter a value</p>`
+  volumeResultEl.innerHTML = `<p>Please Enter a value</p>`
+  massResultEl.innerHTML = `<p>Please Enter a value</p>`
+  inputValueEl.value = 0
+  errorPar.innerHTML = `<p></p>`
+  errorPar.innerHTML = `<p>Enter a value between 0 and 1000</p>`
+  errorPar.style.color = "whitesmoke";
+  errorPar.style.textShadow = "0px 0px 0px black";
 })
